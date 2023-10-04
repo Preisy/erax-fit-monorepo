70bf65d6d992e93b968f7c0e23d987c50eab8efa
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-04 19:45:51 +0300

    ✨ Begin of WFooter + navbar

---
+++ front-body-blast/src/widgets/WFooter/FooterNavLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+  title: string;
+}>();
+</script>
+
+<template>
+  <a text-center>
+    <q-icon :name="icon" />
+    <p>{{ title }}</p>
+  </a>
+</template>
