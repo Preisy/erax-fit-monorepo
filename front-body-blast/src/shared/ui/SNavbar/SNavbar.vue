f543ed3cc5b4f06bd22bbdbd0d5ee63ee9550124
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-04 19:45:51 +0300

    ✨ Begin of WFooter + navbar

---
+++ front-body-blast/src/shared/ui/SNavbar/SNavbar.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <div flex-row>
+        <slot></slot>
+    </div>
+</template>
---
+++ front-body-blast/src/widgets/WFooter/WFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SNavbar } from 'shared/ui/SNavbar';
+import FooterNavLink from './FooterNavLink.vue'
+
+const links = {}
+</script>
+
+<template>
+    <div bg-bg>
+        <SNavbar>
+            <FooterNavLink />
+        </SNavbar>
+    </div>
+</template>
