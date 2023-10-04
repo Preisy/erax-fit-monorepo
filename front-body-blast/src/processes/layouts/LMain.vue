32aefec03554d02f8512ccf31c83e9a362ed7e40
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-04 23:45:05 +0300

    💄 icons, WFooter improve

--- front-body-blast/src/processes/layouts/LMain.vue
+++ front-body-blast/src/processes/layouts/LMain.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import PTraining from 'pages/PTraining.vue';
+import { WFooter } from 'widgets/WFooter';
 </script>
 
 <template>
   <q-layout view="hHh lpr fFf">
     <PTraining />
+    <WFooter />
   </q-layout>
 </template>
---
+++ front-body-blast/src/widgets/WFooter/ui/WFooter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { SNavbar } from 'shared/ui/SNavbar';
+import * as icons from '../assets'
+import FooterNavLink, { FooterNavLinkProps } from './FooterNavLink.vue';
+
+const links: FooterNavLinkProps[] = [
+  { imgSrc: icons.exercise, title: 'Тренировки' },
+  { imgSrc: icons.profile, title: 'Профиль' },
+  { imgSrc: icons.diary, title: 'Дневник' },
+  { imgSrc: icons.diet, title: 'Питание' },
+];
+</script>
+
+<template>
+  <div
+    fixed
+    bottom-0
+    left-0
+    right-0
+    rounded-t-2rem
+    bg-bg
+    boxshadow-footer  
+    class="w-footer"
+  >
+    <SNavbar>
+      <FooterNavLink
+        v-for="navlink of links"
+        :key="navlink.imgSrc"
+        v-bind="navlink"
+      />
+    </SNavbar>
+  </div>
+</template>
