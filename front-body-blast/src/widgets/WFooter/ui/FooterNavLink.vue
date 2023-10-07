4ecc7f0ec0ea698b7451925cda6aea8bf0f918be
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-07 16:01:06 +0300

    💄 WFooter fix

--- front-body-blast/src/widgets/WFooter/ui/FooterNavLink.vue
+++ front-body-blast/src/widgets/WFooter/ui/FooterNavLink.vue
@@ -2,16 +2,31 @@
 export interface FooterNavLinkProps {
   imgSrc: string;
   title: string;
+  href: string;
 }
 defineProps<FooterNavLinkProps>();
 </script>
 
 <template>
-  <a text-center>
+  <a
+    :href="href"
+    
+    max-w-5rem
+    flex
+    flex-1
+    flex-col
+    items-center
+    justify-center
+    gap-0.5rem
+    py-1rem
+    text-center 
+  >
     <q-img
       :src="imgSrc"
-      text-md
+      pointer-events-none
+      w-1.5rem
+      select-none 
     />
-    <p>{{ title }}</p>
+    <span>{{ title }}</span>
   </a>
 </template>
