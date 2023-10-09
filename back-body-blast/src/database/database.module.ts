f1cb8f43bc7b212a76f42b58477bf1ff09fd4718
Jacobzzzzzzz <yakov.komar@mail.ru>
2023-10-09 16:38:28 +0300

    :bug: Fixed dockerfile (only db)
    Backend container and env variables are not working at least for now

--- back-body-blast/src/database/database.module.ts
+++ back-body-blast/src/database/database.module.ts
@@ -4,17 +4,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '../../../.env'}),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         autoLoadEntities: true,
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_TITLE'),
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin',
+        database: 'bb_db',
+        //host: configService.get('DB_HOST'),
+        //port: configService.get('DB_PORT'),
+        //username: configService.get('DB_USER'),
+        //password: configService.get('DB_PASSWORD'),
+       // database: configService.get('DB_TITLE'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: false,
