e34ae89dbc9c7351a06479094ca00f73b8ed25e2
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-10 14:05:21 +0300

    ✨ SwiperJs lib, new: SSwiper SSwiperSlide

---
+++ front-body-blast/src/app/boot/swiper.ts
@@ -0,0 +1,9 @@
+import { boot } from 'quasar/wrappers';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// register Swiper custom elements
+export default boot(async (/* { app, router, ... } */) => {
+  
+});
