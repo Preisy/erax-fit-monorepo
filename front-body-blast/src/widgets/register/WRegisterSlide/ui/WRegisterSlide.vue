3ec99fa58440ee1e33186a0d4d5a73a8a560c252
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-12 22:27:11 +0300

    🩹 splide fix

---
+++ front-body-blast/src/widgets/register/WRegisterSlide/ui/WRegisterSlide.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+import { FForm, FFormProps } from 'features/FForm';
+
+export interface WRegisterSlideProps extends FFormProps{}
+
+defineProps<WRegisterSlideProps>()
+</script>
+<template>
+  <div py-1.5rem>
+    <FForm
+      :action="action"
+      :fields="fields"
+    />
+  </div>
+</template>
