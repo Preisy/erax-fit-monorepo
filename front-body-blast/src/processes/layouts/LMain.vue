b373065013bada6a8d155c72220d1d931a1cbd4b
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-15 22:20:25 +0300

    🚨 💩 eslint --fix

--- front-body-blast/src/processes/layouts/LMain.vue
+++ front-body-blast/src/processes/layouts/LMain.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import PTraining from 'pages/PTraining.vue';
 import { WFooter } from 'widgets/WFooter';
 </script>
 
 <template>
   <q-layout view="hHh lpr fFf" px-0.5rem>
     <q-page-container h-screen>
-      <PTraining />
+      <RouterView />
     </q-page-container>
     <WFooter />
   </q-layout>
