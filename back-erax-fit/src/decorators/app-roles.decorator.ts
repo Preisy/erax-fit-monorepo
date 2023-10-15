dc87c8bbc45988a12e91698862516f2ea41020b4
Jacobzzzzzzz <yakov.komar@mail.ru>
2023-10-15 22:05:05 +0300

    :sparkles: Integrated utils update

--- back-erax-fit/src/decorators/app-roles.decorator.ts
+++ back-erax-fit/src/decorators/app-roles.decorator.ts
@@ -1,6 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { UserRole } from 'src/constants/constants';
-import { RoleGuard } from 'src/modules/authentication/guards/role.guard';
+import { RoleGuard } from 'src/authentication/guards/role.guard';
 
 export const AppRoles = (...roles: UserRole[]) => {
   return UseGuards(RoleGuard(roles));
