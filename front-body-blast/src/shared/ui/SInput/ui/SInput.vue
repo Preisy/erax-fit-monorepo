610a76c7f3d9b8d42c19e9a790cf7d6704b1f321
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-24 18:46:59 +0300

    ✨ 💩 SInput error messagfe

--- front-body-blast/src/shared/ui/SInput/ui/SInput.vue
+++ front-body-blast/src/shared/ui/SInput/ui/SInput.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { QInput, QInputProps } from 'quasar';
 import { useField } from 'vee-validate';
+import ErrorMsg from './ErrorMsg.vue';
 export interface SInputProps extends Omit<QInputProps, 'modelValue' | 'name' | 'placeholder'> {
   placeholder?: string;
   name: string;
@@ -9,22 +10,29 @@ export interface SInputProps extends Omit<QInputProps, 'modelValue' | 'name' | '
 const props = defineProps<SInputProps>();
 const wrapper = ref<HTMLElement>();
 
-const { value } = useField<string | number | undefined>(() => props.name);
+const { value, errorMessage } = useField<string | number | undefined>(() => props.name);
 </script>
 <template>
   <div
     ref="wrapper"
-    class="s_input [&_.q-field\_\_label]:(top-0 text-bg) [&_.q-field\_\_bottom]:display-none [&_.q-field\_\_control]:(h-auto rounded-1rem bg-primary/50 px-1.25rem py-1rem) [&_.not\_empty_.q-field\_\_control]:(bg-primary) [&_.q-field--highlighted_.q-field\_\_control]:(bg-primary) [&_.q-placeholder]:p-0 [&_.q-field]:pb-0 [&_input]:(p-0! text-base! text-bg!) [&_.not\_empty_.q-placeholder]:pt-0.5rem!"
+    bg="primary/50"
+    class="s_input [&_.q-field\_\_label]:(top-0 text-bg) [&_.q-field\_\_bottom]:display-none [&_.q-field\_\_control]:(h-auto px-1.25rem py-1rem transition-all-300) [&_.q-field--highlighted_.q-field\_\_control]:(bg-primary) [&.not\_empty]:(bg-primary) [&_.q-placeholder]:p-0 [&_.q-field]:pb-0 [&.error_.q-field\_\_control]:pb-2rem [&_input]:(p-0! text-base! text-bg!) [&_.q-field--highlighted_.q-placeholder]:pt-0.5rem! [&.not\_empty_.q-placeholder]:pt-0.5rem!"
+    relative
     overflow-hidden
+    rounded-1rem
+    transition-all-300
+    :class="{ not_empty: !!value, error: !!errorMessage }"
   >
     <q-input
       v-bind="{ ...$props }"
       v-model="value"
       standout
       dense
+      :bottom-slots="false"
       :autocomplete="autocomplete"
       :placeholder="placeholder"
-      :class="{ not_empty: !!value }"
+      transition-all-300
     />
+    <ErrorMsg absolute bottom-1rem left-1.25rem z-1 v-if="!!errorMessage" :msg="errorMessage" />
   </div>
 </template>
