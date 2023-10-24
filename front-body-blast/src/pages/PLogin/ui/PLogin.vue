2471f46347b0b32194b0fcb36bb5de6ad431bedd
max2556 <46759632+max2556@users.noreply.github.com>
2023-10-24 16:55:56 +0300

    🩹 Better NavList, PLogin. Paddings

--- front-body-blast/src/pages/PLogin/ui/PLogin.vue
+++ front-body-blast/src/pages/PLogin/ui/PLogin.vue
@@ -4,7 +4,7 @@ import { SStructure } from 'shared/ui/SStructure';
 </script>
 
 <template>
-  <SStructure h- relative flex items-center justify-center>
+  <SStructure relative mt--6.5rem h-screen flex items-center justify-center>
     <ELoginForm w-full />
   </SStructure>
 </template>
