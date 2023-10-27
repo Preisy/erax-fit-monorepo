a323ccbe64b1d8d171e26466316472e4da3e0513
NixNi <nsntmp@gmail.com>
2023-10-28 00:51:31 +0300

    feat: create SDatePicker

--- front-body-blast/src/entities/profile/EAthropometricsSlide/ui/EAthropometricsSlide.vue
+++ front-body-blast/src/entities/profile/EAthropometricsSlide/ui/EAthropometricsSlide.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
 import { toTypedSchema } from '@vee-validate/zod';
-import { QIcon, QDate, date } from 'quasar';
+
 import { Athropometrics } from 'shared/api/profile';
 import { SForm } from 'shared/ui/SForm';
 import { SInput } from 'shared/ui/SInput';
 
-export interface IEAthropometricsSlide {
+export interface EAthropometricsSlideProps {
   editable: boolean;
   tdate: string;
-  panel: string;
   weight?: number;
   waist?: number;
   underbelly?: number;
   shoulder?: number;
   hip?: number;
   hipVolume?: number;
-  dates: string[];
 }
+
 const fields = {
   weight: 'Вес',
   waist: 'Талия',
@@ -25,51 +24,16 @@ const fields = {
   hip: 'Бедро',
   hipVolume: 'Объем бедер',
 };
-const today = date.formatDate(Date.now(), 'DD.MM');
-const getDate = (td: string) => {
-  return td != today ? td : 'Сегодня';
-};
-const props = defineProps<IEAthropometricsSlide>();
-const emit = defineEmits<{
-  (e: 'update:panel', newValue: string): void;
-}>();
-const showDateModal = ref(false);
-const tdate = ref(props.tdate);
 
-watch(tdate, (newDate: string) => {
-  if (tdate !== undefined) {
-    tdate.value = newDate;
-    showDateModal.value = false;
-    setTimeout(() => {
-      emit('update:panel', newDate);
-    }, 0);
-  }
-});
+defineProps<EAthropometricsSlideProps>();
+
 const schema = toTypedSchema(Athropometrics.validation());
 </script>
 
 <template>
-  <div mt-7 flex items-center justify-center gap-2 @click="showDateModal = true">
-    <p text-center font-800>{{ getDate(tdate) }}</p>
-    <q-icon name="calendar_month" text-black />
-  </div>
-  <q-date
-    absolute
-    left="1/2"
-    top="1/2"
-    translate-x="-1/2"
-    translate-y="-1/2"
-    z-100
-    v-model="tdate"
-    v-if="showDateModal"
-    mask="DD.MM"
-    :options="dates"
-  />
-
   <SForm v-if="editable" :action="() => {}" :field-schema="schema" class="[&>div]:(grid grid-cols-2 mt-8 gap-2)">
     <SInput v-for="(value, key) in fields" :key="key" :name="key" :label="value" :value="weight" centered />
   </SForm>
-
   <div v-else grid grid-cols-2 mt-8 gap-2>
     <div
       class="relative rounded-1rem bg-primary px-6 py-4 text-center text-bg"
--- front-body-blast/src/widgets/profile/WAthropometrics/ui/WAthropometrics.vue
+++ front-body-blast/src/widgets/profile/WAthropometrics/ui/WAthropometrics.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { date, QTabPanels, QTabPanel } from 'quasar';
-import { EAthropometricsSlide, IEAthropometricsSlide } from 'entities/profile/EAthropometricsSlide';
+import { EAthropometricsSlide, EAthropometricsSlideProps } from 'entities/profile/EAthropometricsSlide';
+import { SDatePicker } from 'shared/ui/SDatePicker';
 const today = date.formatDate(Date.now(), 'DD.MM');
 const panel = ref(today);
 
 export interface WAthropometrics {
-  slides: Omit<IEAthropometricsSlide, 'panel' | 'dates'>[];
+  slides: Omit<EAthropometricsSlideProps, 'panel' | 'dates'>[];
 }
 const props = defineProps<WAthropometrics>();
 const dates = props.slides.map(
@@ -14,11 +15,10 @@ const dates = props.slides.map(
 </script>
 
 <template>
-  <div>
-    <q-tab-panels v-model="panel" mx="[-0.5rem]" animated swipeable infinite static class="[&_.q-tab-panel]:(p-2)">
-      <q-tab-panel v-for="slide in slides" :name="slide.tdate" :key="slide.tdate">
-        <EAthropometricsSlide v-bind="slide" v-model:panel="panel" :dates="dates" />
-      </q-tab-panel>
-    </q-tab-panels>
-  </div>
+  <SDatePicker v-model:panel="panel" :dates="dates" />
+  <q-tab-panels v-model="panel" mx="[-0.5rem]" animated swipeable infinite static class="[&_.q-tab-panel]:(p-2)">
+    <q-tab-panel v-for="slide in slides" :name="slide.tdate" :key="slide.tdate">
+      <EAthropometricsSlide v-bind="slide" v-model:panel="panel" :dates="dates" />
+    </q-tab-panel>
+  </q-tab-panels>
 </template>
