6f8ad79ce11dd10f6c9566b1d63abd0152491184
SteklotaraBlizz <zuxxlyes2015@gmail.com>
2023-11-06 20:08:39 +0300

    :sparkles: Client video controller

--- back-erax-fit/src/modules/client/bonus-video/client-bonus-video.controller.ts
+++ back-erax-fit/src/modules/client/bonus-video/client-bonus-video.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, UseFilters, UsePipes, ValidationPipe, Get, Req, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { MainExceptionFilter } from '../../../exceptions/main-exception.filter';
 import { AppAuthGuard } from '../../../modules/authentication/guards/appAuth.guard';
 import { ClientBonusVideoService } from './client-bonus-video.service';
+import { AppResponses } from '../../../decorators/app-responses.decorator';
+import { AppSingleResponse } from '../../../dto/app-single-response.dto';
+import { BonusVideoEntity } from '../../../modules/core/bonus-video/entities/bonus-video.entity';
+import { RequestWithUser } from '../../../modules/authentication/types/requestWithUser.type';
 
 @Controller('bonus-video')
 @ApiTags('Client bonus video')
@@ -11,4 +15,10 @@ import { ClientBonusVideoService } from './client-bonus-video.service';
 @UsePipes(ValidationPipe)
 export class ClientBonusVideoController {
   constructor(private readonly clientService: ClientBonusVideoService) {}
+
+  @Get(':id')
+  @AppResponses({ status: 200, type: AppSingleResponse.type(BonusVideoEntity) })
+  async getOne(@Req() req: RequestWithUser, @Param('link') link: string) {
+    return await this.clientService.findOneForUser(req.user.id, link);
+  }
 }
