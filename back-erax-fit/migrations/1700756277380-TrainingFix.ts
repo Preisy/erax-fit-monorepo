7b4f8936900eb4cc0ca38e8d2e8b774f2dff09ac
SteklotaraBlizz <zuxxlyes2015@gmail.com>
2023-11-23 20:57:13 +0300

    :bug: Fixed migration

--- back-erax-fit/migrations/1700756277380-TrainingFix.ts
+++ back-erax-fit/migrations/1700756277380-TrainingFix.ts
@@ -5,9 +5,10 @@ export class TrainingFix1700756277380 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "users" DROP CONSTRAINT "FK_d98a275f8bc6cd986fcbe2eab01"');
-    await queryRunner.query('ALTER TABLE "exercises" ADD "temp" character varying(50) NOT NULL');
+    await queryRunner.query('ALTER TABLE "exercises" ADD "temp" character varying(50)');
     await queryRunner.query('UPDATE "exercises" SET "temp" = "repetitions"::character varying(50)');
     await queryRunner.query('ALTER TABLE "exercises" DROP COLUMN "repetitions"');
+    await queryRunner.query('ALTER TABLE "exercises" ALTER COLUMN "temp" SET NOT NULL');
     await queryRunner.query('ALTER TABLE "exercises" RENAME COLUMN "temp" TO "repetitions"');
     await queryRunner.query(
       'ALTER TABLE "users" ADD CONSTRAINT "FK_d98a275f8bc6cd986fcbe2eab01" FOREIGN KEY ("tokenId") REFERENCES "tokens"("id") ON DELETE SET NULL ON UPDATE CASCADE',
@@ -17,8 +18,9 @@ export class TrainingFix1700756277380 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "users" DROP CONSTRAINT "FK_d98a275f8bc6cd986fcbe2eab01"');
     await queryRunner.query('ALTER TABLE "exercises" RENAME COLUMN "repetitions" TO "temp"');
-    await queryRunner.query('ALTER TABLE "exercises" ADD "repetitions" integer NOT NULL');
+    await queryRunner.query('ALTER TABLE "exercises" ADD "repetitions" integer');
     await queryRunner.query('UPDATE "exercises" SET "repetitions" = "temp"::integer');
+    await queryRunner.query('ALTER TABLE "exercises" ALTER COLUMN "repetitions" SET NOT NULL');
     await queryRunner.query('ALTER TABLE "exercises" DROP COLUMN "temp"');
     await queryRunner.query(
       'ALTER TABLE "users" ADD CONSTRAINT "FK_d98a275f8bc6cd986fcbe2eab01" FOREIGN KEY ("tokenId") REFERENCES "tokens"("id") ON DELETE SET NULL ON UPDATE CASCADE',
