4a5fc622384008d670953464c791755377997de3
Nikita Nikiforov <111698816+NixNi@users.noreply.github.com>
2023-11-28 16:48:40 +0300

    🎉 start Learning block add WVideo.vue

--- front-body-blast/src/pages/PLearning.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>PLearning</div>
-</template>
---
+++ front-body-blast/src/pages/PLearning/ui/PLearning.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { WVideo } from 'widgets/learning/WVideo';
+const props = {
+  title: 'Мышцы груди',
+  links: ['https://v3.cdnpk.net/videvo_files/video/free/2013-08/large_watermarked/hd0983_preview.mp4'],
+  poster: '',
+};
+</script>
+
+<template>
+  <div>
+    <WVideo v-bind="props" />
+  </div>
+</template>
