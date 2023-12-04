1c809bf9610f1c24eaefe53648518bd0cf1decd1
max2556 <46759632+max2556@users.noreply.github.com>
2023-12-04 15:51:03 +0300

    🩹 💩 lint

--- front-body-blast/src/shared/lib/utils/types.ts
+++ front-body-blast/src/shared/lib/utils/types.ts
@@ -7,13 +7,13 @@ type ZodEffectsInner<T extends ZodEffects<ZodTypeAny>> = ZodInner<ReturnType<T['
 export type GetZodInnerType<T> = T extends ValidationFn
   ? GetValidationVnInnerType<T> //If provided <namespace>.validation() fn, then get its inner
   : T extends ZodEffects<ZodTypeAny>
-  ? ZodEffectsInner<T> // if provided ZodEffects - take its inner
-  : T extends ZodObject<ZodRawShape> | ZodTypeAny
-  ? ZodInner<T> // if provided ZodObject or ZodTypeAny - take its inner
-  : never;
+    ? ZodEffectsInner<T> // if provided ZodEffects - take its inner
+    : T extends ZodObject<ZodRawShape> | ZodTypeAny
+      ? ZodInner<T> // if provided ZodObject or ZodTypeAny - take its inner
+      : never;
 
 type GetValidationVnInnerType<T extends ValidationFn> = ReturnType<T> extends ZodEffects<ZodTypeAny>
   ? ZodEffectsInner<ReturnType<T>> // if validation() returns ZodEffect, take its inner type
   : ReturnType<T> extends ZodObject<ZodRawShape> | ZodTypeAny //If validation returns ZodObject or ZodType
-  ? ZodInner<ReturnType<T>> //Take its inner type
-  : never; // If validation() returns other -> never
+    ? ZodInner<ReturnType<T>> //Take its inner type
+    : never; // If validation() returns other -> never
